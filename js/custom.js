e280d442be0e4eb985db002321f069ef5622be89
skurilyak <kurilyak@gmail.com>
2016-11-10 14:18:42 -0800

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-39583019-10', 'auto');
+ga('send', 'pageview');
+
+// Hotjar Analytics
+(function(h,o,t,j,a,r){
+  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+  h._hjSettings={hjid:204824,hjsv:5};
+  a=o.getElementsByTagName('head')[0];
+  r=o.createElement('script');r.async=1;
+  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window,document,'http://static.hotjar.com/c/hotjar-','.js?sv=');
+
+// Smooch.io Web Chat
+Smooch.init({
+  appToken: '1bg1abtol7v7fnsdaumlw02j4',
+  emailCaptureEnabled: true,
+});
