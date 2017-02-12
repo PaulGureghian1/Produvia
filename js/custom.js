07986ec372fdfa19d3c5ca0a8e6f25810332412a
skurilyak <kurilyak@gmail.com>
2017-02-11 20:33:28 -0800

    Removed redundant Google Analytics script

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,10 @@
 // Google Analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-39583019-10', 'auto');
-ga('send', 'pageview');
+// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+// })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+// ga('create', 'UA-39583019-10', 'auto');
+// ga('send', 'pageview');
 
 // Hotjar Analytics
 (function(h,o,t,j,a,r){
