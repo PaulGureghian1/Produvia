5f49cb255a25a252c36579c9f5669710299284d1
skurilyak <kurilyak@gmail.com>
2017-03-05 08:41:22 -0800

    Added code comments

--- js/custom.js
+++ js/custom.js
@@ -14,22 +14,7 @@ analytics.load("CtsIiKDoEvbyUVdYsEmtcrOVsvG7kIMY");
 analytics.page();
 }}();
 
-// Amplitude
-(function(e,t){var n=e.amplitude||{_q:[],_iq:{}};var r=t.createElement("script");r.type="text/javascript";
-r.async=true;r.src="https://d24n15hnbwhuhn.cloudfront.net/libs/amplitude-3.0.1-min.gz.js";
-r.onload=function(){e.amplitude.runQueuedFunctions()};var i=t.getElementsByTagName("script")[0];
-i.parentNode.insertBefore(r,i);function s(e,t){e.prototype[t]=function(){this._q.push([t].concat(Array.prototype.slice.call(arguments,0)));
-return this}}var o=function(){this._q=[];return this};var a=["add","append","clearAll","prepend","set","setOnce","unset"];
-for(var u=0;u<a.length;u++){s(o,a[u])}n.Identify=o;var c=function(){this._q=[];return this;
-};var p=["setProductId","setQuantity","setPrice","setRevenueType","setEventProperties"];
-for(var l=0;l<p.length;l++){s(c,p[l])}n.Revenue=c;var d=["init","logEvent","logRevenue","setUserId","setUserProperties","setOptOut","setVersionName","setDomain","setDeviceId","setGlobalUserProperties","identify","clearUserProperties","setGroup","logRevenueV2","regenerateDeviceId"];
-function v(e){function t(t){e[t]=function(){e._q.push([t].concat(Array.prototype.slice.call(arguments,0)));
-}}for(var n=0;n<d.length;n++){t(d[n])}}v(n);n.getInstance=function(e){e=(!e||e.length===0?"$default_instance":e).toLowerCase();
-if(!n._iq.hasOwnProperty(e)){n._iq[e]={_q:[]};v(n._iq[e])}return n._iq[e]};e.amplitude=n;
-})(window,document);
-
-amplitude.getInstance().init("6e37ef5b6d5f144f750ddfc716518af1");
-
+// Segment - Track Top Menu Clicks
 $( document ).ready(function() {
 	$( ".menu-home" ).click(function() {
 		analytics.track("CLICKED_HOME", {});
@@ -58,9 +43,9 @@ $( document ).ready(function() {
 	$( ".menu-start-project-about-us" ).click(function() {
 		analytics.track("CLICKED_START_PROJECT_FROM_ABOUT_US", {});
 	});
-
 });
 
+// Segment - Store User Email using Intercom
 $(window).load(function() {
 	var iFrameCheckIntId = setInterval(function(){
 		if($('.intercom-launcher-frame').length > 0) {
@@ -86,6 +71,7 @@ $(window).load(function() {
 	},100);
 });
 
+// Segment - Generate GUID
 function guid() {
   function s4() {
     return Math.floor((1 + Math.random()) * 0x10000)
